7da5465b7cee562889339b7486c71a59b14fdbd1
hur15022 <hur15022@gmail.com>
2016-07-12 19:38:09 -0600

    Add files via upload

---
+++ iRadio.java
@@ -0,0 +1,27 @@
+/**
+*@author German Garcia 15008, Jose Martinez 15163, Diego Hurtarte 15022
+*@version 14.07.16
+*/
+/**
+* Universidad del Valle de Guatemala, Estructura de Datos, Seccion 20
+*/
+
+/*
+* iRadio es la encargada de definir los metodos que seran utilizados, crea la interface
+*/
+
+public interface iRadio {
+
+	public void on();
+	public void off();
+	public String isOn();
+	public String getFrecuency();
+	public void setFrecuency(String frecuency);
+	public String getStation();
+	public void setStation(String station);
+	public void forward();
+	public void backward();
+	public void setMemory(int position);
+	public String toString(String frecuency, String station);
+}
+
